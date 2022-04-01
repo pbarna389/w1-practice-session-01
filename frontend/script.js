0c68f15baf44c9f2a862fabc898065146efbdffc
pbarna389 <64441402+pbarna389@users.noreply.github.com>
2022-04-01 12:25:22 +0200

    Changes made

---
+++ frontend/script.js
@@ -0,0 +1,33 @@
+console.log("Ez az első Javascript-em");
+
+/*
+idézőjelbe kell raknunk a string-eket
+ "Ez itt egy szöveg";
+
+a dupla és sima codemark-ok csak egy soros szöveget engednek meg
+ 'Ez itt egy másik szöveg';
+
+backtick-ek közé mehet a sortörés
+ `Ez egy több 
+
+ soros szöveg(string)`
+
+
+a számokat nem kell idézőjelbe rakni
+ 4
+ 24
+ 128
+ 5555
+ 00035346246
+ 03
+ 04
+ 56
+ 1.13
+ 0.42
+*/
+
+function myFirstFunction() {
+    console.log(`Ez az első függvényem belsejében lévő log`);
+};
+
+myFirstFunction();
